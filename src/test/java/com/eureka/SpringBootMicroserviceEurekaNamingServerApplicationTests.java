535f31aa6f5769f2f3857a3f68268b2cf4b7f174
sandip vaghasiya <hi.shah31@gmail.com>
2019-12-16 10:20:22 +0100

    inital commit

---
+++ src/test/java/com/eureka/SpringBootMicroserviceEurekaNamingServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.eureka;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootMicroserviceEurekaNamingServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
